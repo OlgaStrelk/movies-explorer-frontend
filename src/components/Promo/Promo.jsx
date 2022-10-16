875132511f87afb52c6229ad3b651c91c3c12649
Strelkova Olga <strelod@yandex.ru>
2022-10-17 02:45:00 +0300

    feat: promo 1280px

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,8 +1,12 @@
-import './Promo.css';
-
+import "./Promo.css";
 function Promo() {
   return (
-    <></>
+    <div className="promo__container">
+      <div className="promo__image"></div>
+      <h1 className="promo__title">
+        Учебный проект студентки факультета Веб-разработки.
+      </h1>
+    </div>
   );
 }
 
