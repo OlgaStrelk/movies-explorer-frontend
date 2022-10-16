270574940ac1fd6307312495915ce64f8a8158ca
Strelkova Olga <strelod@yandex.ru>
2022-10-17 01:24:40 +0300

    feat: promo header 1280px

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,16 @@
+import React from 'react';
 import './Header.css';
+import { ReactComponent as LogoImage } from '../../images/header__logo.svg';
 
 function Header() {
   return (
-    <></>
+    <div className="header__panel header__promo-panel">
+			<LogoImage />
+		<div className='header__btns'>
+			<button type='button' className='header__btn header__signup-btn'>Регистрация</button>
+			<button type='button' className='header__btn header__signin-btn'>Войти</button>
+		</div>
+	</div>
   );
 }
 
