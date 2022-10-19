879e767e36074717e16d61542a0a79581618f88a
Strelkova Olga <strelod@yandex.ru>
2022-10-19 11:31:35 +0300

    refactor: portfolio data

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,14 +1,16 @@
 import "./Portfolio.css";
-import ListItem from '../ListItem/ListItem';
+import ListItem from "../ListItem/ListItem";
 
 function Portfolio() {
   const PORTFOLIO_TITLE = "Портфолио";
   const LINKS_DATA = [
-    { id: 1, href: "", title: "Статичный сайт" },
-    { id: 2, href: "", title: "Адаптивный сайт" },
-    { id: 3, href: "", title: "Одностраничное приложение" },
+    { id: 1, data: { href: "", title: "Статичный сайт" } },
+    { id: 2, data: { href: "", title: "Адаптивный сайт" } },
+    { id: 3, data: { href: "", title: "Одностраничное приложение" } },
   ];
-  const linksMarkdown = LINKS_DATA.map((link) => <ListItem link={link} />);
+  const linksMarkdown = LINKS_DATA.map((link) => (
+    <ListItem key={link.id} link={link.data} />
+  ));
   return (
     <>
       <h4 className="portfolio__title">{PORTFOLIO_TITLE}</h4>
