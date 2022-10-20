5d16970825902fc9b2ba00cc06cab27b37b86ba4
Strelkova Olga <strelod@yandex.ru>
2022-10-20 17:23:35 +0300

    refactor: header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import "./Header.css";
 import { ReactComponent as LogoImage } from "../../images/header__logo.svg";
 import AuthNavigation from "../AuthNavigation/AuthNavigation";
-import LoggedInNavigation from '../LoggedInNavigation/LoggedInNavigation';
+import LoggedInNavigation from "../LoggedInNavigation/LoggedInNavigation";
 
 function Header({ isLoggedIn, handler }) {
+  const STYLE = {
+    section: "section",
+    sectionType: { promo: "section_type_promo" },
+    header: "header__panel",
+  };
+
   return (
-    <div className="section section_type_promo header__panel">
+    <div
+      className={
+        isLoggedIn
+          ? `${STYLE.section} ${STYLE.header}`
+          : `${STYLE.section} ${STYLE.sectionType.promo} ${STYLE.header}`
+      }
+    >
       <LogoImage onClick={handler} />
       {!isLoggedIn ? <AuthNavigation /> : <LoggedInNavigation />}
     </div>
--- src/components/LoggedInNavigation/LoggedInNavigation.jsx
+++ src/components/LoggedInNavigation/LoggedInNavigation.jsx
@@ -24,7 +24,7 @@ function LoggedInNavigation() {
   return (
     <>
       <Navigation data={BTNS_DATA} />
-      {/* <AccountLink></AccountLink> */}
+      {/* <AccountLink /> */}
     </>
   );
 }
