e25cc86d9268ac2101de8d8f183104516a73e348
Strelkova Olga <strelod@yandex.ru>
2022-11-01 17:53:02 +0300

    feat: Profile components

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,16 @@
 import "./Profile.css";
+import { useEffect } from 'react';
+import ProfileForm from '../ProfileForm/ProfileForm';
 
 function Profile(props) {
+  useEffect(() => {
+    props.handler();
+  }, []);
+
   return (
-    <>
-    </>
+    <main className='profile'>
+      <ProfileForm />
+    </main>
   );
 }
 
---
+++ src/components/ProfileForm/ProfileForm.jsx
@@ -0,0 +1,7 @@
+import "./ProfileForm.css";
+
+function ProfileForm(props) {
+  return <form className="profile-form"></form>;
+}
+
+export default ProfileForm;
