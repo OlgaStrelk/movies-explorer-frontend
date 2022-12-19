2e5b0198b070af010b2f52b28f5008446a1a32b9
“Olga <strelod@yandex.ru>
2022-12-19 13:19:32 -0800

    feat: validation with Joi

--- src/hooks/useEmailValidation.js
+++ src/hooks/useEmailValidation.js
@@ -1,19 +1,20 @@
-// import Joi from "joi";
+import Joi from "joi";
 
-// import {
-//   INPUT_IS_REQUIRED_TEXT,
-//   INVALID_EMAIL_TEXT,
-// } from "../utils/errorTexts";
+import {
+  INPUT_IS_REQUIRED_TEXT,
+  INVALID_EMAIL_TEXT,
+} from "../utils/errorTexts";
 
-// import { useCallback } from "react";
-// import { MAX_LENGTH, MIN_LENGTH } from "../utils/consts";
+import { useCallback } from "react";
+import { MAX_LENGTH, MIN_LENGTH } from "../utils/consts";
 
 // export const useJoiValidationResolver = (validationSchema) =>
 //   useCallback(
 //     async (data) => {
 //       try {
-//         const result = await validationSchema.validate(data, { abortEarly: false });
-//         console.log(`result ${result}`);
+//         console.log(data);
+//         const values = await Joi.validate(data, validationSchema, {abortEarly: false});
+//         console.log(`values ${values}`);
 
 //         return {
 //           values,
@@ -23,57 +24,83 @@
 //         console.log(`errors ${errors}`);
 //         return {
 //           values: {},
-//           errors: errors.inner.reduce(
-//             (allErrors, currentError) => ({
-//               ...allErrors,
-//               [currentError.path]: {
-//                 type: currentError.type ?? "validation",
-//                 message: currentError.message,
-//               },
-//             }),
-//             {}
-//           ),
+//             errors: errors?.inner?.reduce(
+//               (allErrors, currentError) => ({
+//                 ...allErrors,
+//                 [currentError.path]: {
+//                   type: currentError.type ?? "validation",
+//                   message: currentError.message,
+//                 },
+//               }),
+//               {}
+//             ),
 //         };
 //       }
 //     },
 //     [validationSchema]
 //   );
+//().keys
 
-// export const validationSchema = Joi.object().keys({
-//   name: Joi.string()
-//     .alphanum()
-//     .min(MIN_LENGTH)
-//     .max(MAX_LENGTH)
-//     .required()
-//     .error(errors => {
-//       errors.forEach(err => {
-//         switch (err.code) {
-//           case "any.empty":
-//             err.message = "Value should not be empty!";
-//             break;
-//           case "string.min":
-//             err.message = `Value should have at least ${err.local.limit} characters!`;
-//             break;
-//           case "string.max":
-//             err.message = `Value should have at most ${err.local.limit} characters!`;
-//             break;
-//           default:
-//             break;
-//         }
-//       });
-//       return errors;
-//     }),
-    // .messages({
-    //   'string.base': ``,
-    //   'string.empty': ``,
-    //   'string.min': `${MIN_LENGTH}{#limit}`,
-    //   'string.min': `${MAX_LENGTH}{#limit}`,
-    //   'any.required': `${INPUT_IS_REQUIRED_TEXT}`
+export const useJoiValidationResolver = (validationSchema) =>
+  useCallback(
+    async (data) => {
+      try {
+        const values = await validationSchema.validateAsync(data);
+        console.log(values);
+        return {
+          values,
+          errors: {},
+        };
+      } catch (errors) {
+        console.log(errors);
+        return {
+          values: {},
+          errors: errors?.inner?.reduce(
+            (allErrors, currentError) => ({
+              ...allErrors,
+              [currentError.path]: {
+                type: currentError.type ?? "validation",
+                message: currentError.message,
+              },
+            }),
+            {}
+          ),
+        };
+      }
+    },
+    [validationSchema]
+  );
 
-//     }),
+export const validationSchema = Joi.object({
+  name: Joi.string().alphanum().min(MIN_LENGTH).max(MAX_LENGTH).required(),
+  // .error((errors) => {
+  //   errors.forEach((err) => {
+  //     switch (err.code) {
+  //       case "any.empty":
+  //         err.message = "Value should not be empty!";
+  //         break;
+  //       case "string.min":
+  //         err.message = `Value should have at least ${err.local.limit} characters!`;
+  //         break;
+  //       case "string.max":
+  //         err.message = `Value should have at most ${err.local.limit} characters!`;
+  //         break;
+  //       default:
+  //         break;
+  //     }
+  //   });
+  //   return errors;
+  // }),
+  //   .messages({
+  //     'string.base': ``,
+  //     'string.empty': ``,
+  //     'string.min': `${MIN_LENGTH}{#limit}`,
+  //     'string.min': `${MAX_LENGTH}{#limit}`,
+  //     'any.required': `${INPUT_IS_REQUIRED_TEXT}`
 
-//   email: Joi.string()
-//     .email({ tlds: {allow: false}})
-//     .required(),
-//   password: Joi.string().required(),
-// });
+  //   }),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
+  password: Joi.string().required(),
+});
