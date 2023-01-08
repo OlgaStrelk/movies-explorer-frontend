201314972b9b970e2c10b2d85dab3b9dc3a13a3d
“Olga <strelod@yandex.ru>
2023-01-08 15:13:53 -0800

    fix: disabled state

--- src/components/ProfileForm/ProfileForm.jsx
+++ src/components/ProfileForm/ProfileForm.jsx
@@ -84,6 +84,7 @@ function ProfileForm({ logOutHandler, setCurrentUser }) {
         title: "Редактировать",
         uniqueStyle: STYLES_CONFIG.editBtnClassName,
         type: "submit",
+        disabled: (isValid && isDirty) ? false : true
       },
     },
     {
@@ -93,6 +94,7 @@ function ProfileForm({ logOutHandler, setCurrentUser }) {
         uniqueStyle: STYLES_CONFIG.signOutBtnClassName,
         type: "button",
         clickHandler: handleSignOutClick,
+        disabled: false
       },
     },
   ];
@@ -106,7 +108,7 @@ function ProfileForm({ logOutHandler, setCurrentUser }) {
         type={btn.data.type}
         className={(isValid && isDirty) ? btnClassName : disabledBtnClassName}
         onClick={btn.data.clickHandler}
-        disabled={(isValid && isDirty) ? false : true}
+        disabled={btn.data.disabled}
       >
         {btn.data.title}
       </button>
